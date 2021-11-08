6ee115c3c5f2092214eadb5abda1499f20dffcae
pranav <prnvkatkar88@gmail.com>
2021-11-08 18:47:58 +0530

    Added:reads from file and displays on console

--- EmployeePayrollTest.java
+++ EmployeePayrollTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class EmployeePayrollTest {
-	 @Test
+//	 @Test
 	    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
 	        EmployeePayrollTwo employeePayrollService = new EmployeePayrollTwo();
 	        employeePayrollService.addEmployee(new EmployeeData(1, "Jeff Bezos", 10000));
@@ -14,7 +14,7 @@ class EmployeePayrollTest {
 	        assertEquals(3, entries);
 	    }
 	 
-	    @Test
+	//    @Test
 	    public void given3EmployeesWhenWrittenToFileShouldPrintEmployeeEntries()
 	    {
 	        EmployeePayrollTwo employeePayrollService = new EmployeePayrollTwo();
@@ -24,6 +24,13 @@ class EmployeePayrollTest {
 	        employeePayrollService.writeEmployeeDataToFile();
 	        employeePayrollService.printData();
 	        long entries = employeePayrollService.countEntries();
+	        assertEquals(3, entries);
+	    }
+	    
+	    @Test
+	    public void givenFileOnReadindFromFileShouldMatchEmployeeCount() {
+	        EmployeePayrollTwo employeePayrollService = new EmployeePayrollTwo();
+	        long entries = employeePayrollService.readDataFromFile();
 	        assertEquals(3, entries);
 	    }
 }
