2bc6c4ef9cf30af17de367f6c8957882f98158ad
pranav <prnvkatkar88@gmail.com>
2021-11-08 18:20:39 +0530

    Added:Stored employee payroll to a file

---
+++ EmployeePayrollTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeePayrollTest {
+	 @Test
+	    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+	        EmployeePayrollTwo employeePayrollService = new EmployeePayrollTwo();
+	        employeePayrollService.addEmployee(new EmployeeData(1, "Jeff Bezos", 10000));
+	        employeePayrollService.addEmployee(new EmployeeData(2, "Bill Gates", 20000));
+	        employeePayrollService.addEmployee(new EmployeeData(3, "Mark Z", 30000));
+	        employeePayrollService.writeEmployeeDataToFile();
+	        long entries = employeePayrollService.countEntries();
+	        assertEquals(3, entries);
+	    }
+}
