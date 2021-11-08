8372e1db16600bf70c7207b4d3883672fb0983da
pranav <prnvkatkar88@gmail.com>
2021-11-08 17:17:36 +0530

    Added:watch service created to watch directory along with files

---
+++ Java8WatchServiceExample.java
@@ -0,0 +1,91 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Java8WatchServiceExample {
+	private final WatchService watcher;
+	private final Map<WatchKey, Path> dirWatchers;
+
+	/* creates the watch service and registers the given directory */
+	public Java8WatchServiceExample(Path dir) throws IOException {
+		this.watcher = FileSystems.getDefault().newWatchService();
+		this.dirWatchers = new HashMap<WatchKey, Path>();
+		scanAndRegisterDirectories(dir);
+	}
+
+	/* Register the given directory with watch service */
+	private void registerDirWatchers(Path dir) throws IOException {
+		WatchKey key = dir.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE,
+				StandardWatchEventKinds.ENTRY_DELETE);
+		dirWatchers.put(key, dir);
+	}
+
+	/*
+	 * Register the given directory and all its sub directories,with the watch
+	 * service
+	 */
+	private void scanAndRegisterDirectories(final Path start) throws IOException {
+		Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				registerDirWatchers(dir);
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+	
+	/*Process all events for keys queued to the watcher*/
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void processEvents() {
+		while (true) {
+			WatchKey key; //wait for key to be signaled
+			try {
+				key = watcher.take();
+			} catch (InterruptedException e) {
+				return;
+			}
+			Path dir = dirWatchers.get(key);
+			if (dir == null) {
+				continue;
+			}
+			for (WatchEvent<?> event : key.pollEvents()) {
+				WatchEvent.Kind kind = event.kind();
+				Path name = ((WatchEvent<Path>) event).context();
+				Path child = dir.resolve(name);
+				System.out.format("%s: %s\n", event.kind().name(), child);
+				
+				//if directory is created register it and all its sub directories
+				if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+					try {
+						if (Files.isDirectory(child))
+							scanAndRegisterDirectories(child);
+					} catch (IOException e) {
+					}
+				} else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+					if (Files.isDirectory(child)) {
+						dirWatchers.remove(key);
+					}
+				}
+			}
+			
+			//reset key and remove from set if directory no longer accessible
+			boolean valid = key.reset();
+			if (!valid) {
+				dirWatchers.remove(key);
+				if (dirWatchers.isEmpty())
+					break;
+			}
+		}
+	}
+
+}
--- NIOFileAPITest.java
+++ NIOFileAPITest.java
@@ -12,41 +12,51 @@ import junit.framework.Assert;
 
 class NIOFileAPITest {
 	private static String HOME = System.getProperty("user.home");
-	private static String PLAY_WITH_NIO = "TempPlayGround";
+    private static String PLAY_WITH_NIO = "TempPlayGround";
 
-	@Test
-	public void givenPathWhenCHeckedThenConfirm() throws IOException {
-		// check file exists
-		Path homePath = Paths.get(HOME);
-		Assert.assertTrue(Files.exists(homePath));
-		// Delete file and check file not exist
-		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-		if (Files.exists(playPath)) {
-			Files.delete(playPath);
-		}
-		assertTrue(Files.notExists(playPath));
-		// create directory
-		Files.createDirectory(playPath);
-		Assert.assertTrue(Files.exists(playPath));
-		// create file
-		IntStream.range(1, 10).forEach(cntr -> {
-			Path tempFile = Paths.get(playPath + "/temp" + cntr);
-			Assert.assertTrue(Files.notExists(tempFile));
-			try {
-				Files.createFile(tempFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			Assert.assertTrue(Files.exists(tempFile));
-		});
-		// list files,directories as well as files with extension
-		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+    @Test
+    public void givenPathWhenCheckedThenConfirm() throws IOException {
+        Path homePath = Paths.get(HOME);
+        Assert.assertTrue(Files.exists(homePath));
 
-		Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        if (Files.exists(playPath)) {
+            Files.delete(playPath);
+        }
+        Assert.assertTrue(Files.notExists(playPath));
 
-		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
-				.forEach(System.out::println);
+        Files.createDirectory(playPath);
+        Assert.assertTrue(Files.exists(playPath));
 
-	}
+        IntStream.range(1, 10).forEach(cntr -> {
+            Path tempFile = Paths.get(playPath + "/temp" + cntr);
+            Assert.assertTrue(Files.notExists(tempFile));
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Assert.assertTrue(Files.exists(tempFile));
+        });
+
+        Files.list(playPath)
+                .filter(Files::isRegularFile)
+                .forEach(System.out::println);
+
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+
+        Files.newDirectoryStream(playPath,
+                        path -> path.toFile().isFile() &&
+                                path.toString().startsWith("temp"))
+                .forEach(System.out::println);
+
+    }
+
+    @Test
+    public void givenADirectoryWhenWatchedListAllActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
+    }
 
 }
