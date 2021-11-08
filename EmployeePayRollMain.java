5c1e37df41a445cc9a3276fe75cb2b484b1f69ec
pranav <prnvkatkar88@gmail.com>
2021-11-08 12:28:48 +0530

    Added:created an employee payroll service to read and write employee payroll to a console

--- EmployeePayRollMain.java
+++ EmployeePayRollMain.java
@@ -3,7 +3,9 @@ public class EmployeePayRollMain {
 
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO EMPLOYEE PAYROLL SERVICE");
-
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.readEmployeeDataFromConsole();
+		employeePayrollService.writeEmployeeDataInConsole();
 	}
 
 }
