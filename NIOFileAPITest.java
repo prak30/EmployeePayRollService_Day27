6349c51e84e59169e97692d6abb735332721ddd6
pranav <prnvkatkar88@gmail.com>
2021-11-08 13:00:19 +0530

    Added:demonstares file operations like check if file exists,delete file and check file not exist,create directory,create empty file and list files,directories as well as files with extentions

---
+++ NIOFileAPITest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class NIOFileAPITest {
+	private static String HOME = System.getProperty("user.home");
+	private static String PLAY_WITH_NIO = "TempPlayGround";
+
+	@Test
+	public void givenPathWhenCHeckedThenConfirm() throws IOException {
+		// check file exists
+		Path homePath = Paths.get(HOME);
+		Assert.assertTrue(Files.exists(homePath));
+		// Delete file and check file not exist
+		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		if (Files.exists(playPath)) {
+			Files.delete(playPath);
+		}
+		assertTrue(Files.notExists(playPath));
+		// create directory
+		Files.createDirectory(playPath);
+		Assert.assertTrue(Files.exists(playPath));
+		// create file
+		IntStream.range(1, 10).forEach(cntr -> {
+			Path tempFile = Paths.get(playPath + "/temp" + cntr);
+			Assert.assertTrue(Files.notExists(tempFile));
+			try {
+				Files.createFile(tempFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Assert.assertTrue(Files.exists(tempFile));
+		});
+		// list files,directories as well as files with extension
+		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+
+		Files.newDirectoryStream(playPath).forEach(System.out::println);
+
+		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+				.forEach(System.out::println);
+
+	}
+
+}
